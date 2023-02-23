02ced59a4cfdba937c13146d0b39e3f936c43a07
TominoLP <erfinder.tom@gmail.com>
2023-02-23 01:07:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,125 @@
+# 2FA-API
+
+## Introduction
+The TWOFA-API is a Java library for generating QR codes and validating codes for Two-factor authentication. The library supports both the TOTP (Time-based One-Time Password) algorithm and SHA1 hashing. The library generates a secret key that can be stored securely on the client-side and a QR code that can be displayed to the user.
+
+
+---
+
+## Usage
+
+You can use the TWOFA-API library in your Java application by following these steps:
+
+1. Generate a secret key:
+```java
+String secretKey = AuthSys.generateSecretKey();
+```
+
+2. Generate a QR code image or bufferd image:
+```java
+BufferedImage qrCodeImage = AuthSys.generateQrCodeData(secretKey, "YourApp", "username");
+```
+
+```java
+Image qrCodeImage = AuthSys.generateQrCode(secretKey, "YourApp", "username");
+```
+you can also use the method `generateLink` to get the link to the QR code image:
+```java
+String link = AuthSys.generateLink(secretKey, "YourApp", "username");
+```
+
+
+
+
+3. Display the QR code to the user, so they can scan it with their authenticator app,
+such as Google Authenticator
+        
+
+4. Validate the code entered by the user:
+```java
+boolean isValid = AuthSys.validateCode(secretKey, userEnteredCode);
+```
+
+That's it! You can now use the TWOFA-API library to add Two-factor authentication to your Java application.
+
+---
+## Example
+
+```java
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Example {
+    public static void main(String[] args) {
+        String secretKey = AuthSys.generateSecretKey();
+        String issuer = "Example Company";
+        String account = "john@example.com";
+
+        // Generate the QR code as a BufferedImage
+        Image qrCodeImage = AuthSys.generateQrCode(secretKey, issuer, account);
+
+        // Display the QR code image or save it to a file
+
+        // Validate a code
+        String code = "123456"; // The code entered by the user
+        boolean isValid = AuthSys.validateCode(secretKey, code);
+        if (isValid) {
+            System.out.println("Code is valid!");
+        } else {
+            System.out.println("Code is not valid.");
+        }
+    }
+}
+```
+
+---
+
+## Installation
+
+You can install the TWOFA-API library via [jitpack](http://www.jitpack.io) by adding the following to your Maven pom.xml file:
+
+#### Maven
+
+```xml
+<repository>
+    <id>jitpack.io</id>
+    <url>https://jitpack.io</url>
+</repository>
+
+<dependency>
+    <groupId>com.github.TominoLP</groupId>
+    <artifactId>2FA-API</artifactId>
+    <version>v1.1</version>
+</dependency>
+```
+
+#### Gradle
+
+Or, if you're using Gradle, add the following to your build.gradle file:
+
+```kotlin
+repositories {
+    maven { url 'https://jitpack.io' }
+}
+
+dependencies {
+    implementation 'com.github.TominoLP:2FA-API:v1.1'
+}
+```
+
+---
+
+## License
+This API is licensed under the Apache License 2.0. click [here](http://www.apache.org/licenses/LICENSE-2.0) to read the full license.
+
+---
+
+## Support
+**add me on discord:** _Tomino#0101_\
+**or:** [Send an email](mailto:Kontakt@TomWerth.de)
+
+
+---
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ src/main/java/de/tomino/AuthSys.java
@@ -0,0 +1,155 @@
+package de.tomino;/*
+ * Copyright 2023 Tom Werth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import net.glxn.qrgen.javase.QRCode;
+import org.apache.commons.codec.binary.Base32;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Instant;
+
+/**
+ * A class that contains all the methods to generate a QR code and validate the code
+ *
+ * @author TominoLP / Tomino#0101
+ * @version 1.0
+ */
+
+public class AuthSys {
+
+    private static final SecureRandom random = new SecureRandom();
+
+    /**
+     * Generates a random Security Key
+     *
+     * @return the Security Key as a String
+     */
+    public static String generateSecretKey() {
+        byte[] bytes = new byte[20];
+        AuthSys.random.nextBytes(bytes);
+        final Base32 base32 = new Base32();
+        return base32.encodeToString(bytes);
+    }
+
+    /**
+     * Validate the code to check if it is valid
+     *
+     * @param secret The secret key
+     * @param code   The code that should be validated
+     * @return True if the code is valid
+     */
+
+    public static boolean validateCode(String secret, String code) {
+        final Base32 base32 = new Base32();
+        byte[] decodedKey = base32.decode(secret);
+        long time = Instant.now().getEpochSecond() / 30;
+        byte[] data = new byte[8];
+        for (int i = 7; i >= 0; i--) {
+            data[i] = (byte) (time & 0xff);
+            time >>= 8;
+        }
+        final SecretKeySpec signingKey = new SecretKeySpec(decodedKey, "HmacSHA1");
+        Mac mac = null;
+        try {
+            mac = Mac.getInstance("HmacSHA1");
+            mac.init(signingKey);
+        } catch (NoSuchAlgorithmException | InvalidKeyException exception) {
+            exception.printStackTrace();
+        }
+
+        if (mac == null) return false;
+        byte[] hash = mac.doFinal(data);
+        int offset = hash[hash.length - 1] & 0xf;
+        int value = ((hash[offset] & 0x7f) << 24) |
+                ((hash[offset + 1] & 0xff) << 16) |
+                ((hash[offset + 2] & 0xff) << 8) |
+                (hash[offset + 3] & 0xff);
+
+        int digits = 6;
+        int mod = (int) Math.pow(10, digits);
+        return value % mod == Integer.parseInt(code);
+    }
+
+    /**
+     * Generate the QR code from the data as a BufferedImage
+     *
+     * @param secretKey The secret key
+     * @param issuer    The issuer
+     * @param account   The account name
+     * @return The generated BufferedImage
+     * @see BufferedImage
+     */
+
+    public static BufferedImage generateQrCodeData(String secretKey, String issuer, String account) {
+        Image image = generateQrCode(secretKey, issuer, account);
+        final BufferedImage imageData = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB_PRE);
+        final Graphics graphics = imageData.getGraphics();
+        graphics.drawImage(image, 0, 0, 128, 128, null);
+        graphics.dispose();
+
+        return imageData;
+    }
+
+    /**
+     * Generate the QR code from the data as an Image
+     *
+     * @param secretKey The secret key
+     * @param issuer    The issuer
+     * @param account   The account name
+     * @return The generated Image
+     * @see Image
+     */
+    public static Image generateQrCode(String secretKey, String issuer, String account) {
+        final String data = "otpauth://totp/" + account + "?secret=" + secretKey
+                + "&issuer=" + issuer + "&algorithm=SHA1&digits=6&period=30";
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        QRCode.from(data).withCharset("UTF-8").writeTo(stream);
+
+        final InputStream is = new ByteArrayInputStream(stream.toByteArray());
+        Image image = null;
+        try {
+            image = ImageIO.read(is);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return image;
+
+    }
+    
+    /**
+     * Generate the link to the QR code
+     *
+     * @param secretKey The secret key
+     * @param issuer    The issuer
+     * @param account   The account name
+     * @return The generated link
+     */
+    public static String generatelink(String secretKey, String issuer, String account) {
+        return "otpauth://totp/" + account + "?secret=" + secretKey
+                + "&issuer=" + issuer + "&algorithm=SHA1&digits=6&period=30";
+    }
+
+}
